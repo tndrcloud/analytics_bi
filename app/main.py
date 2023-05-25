e6e5011a24b2f21b9a0b46ab8df876a9b8b375ef
Aleksandr Bardusov <96628661+tndrcloud@users.noreply.github.com>
2023-05-25 23:55:53 +0300

    add analytics.py (separation out main.py)

--- app/main.py
+++ app/main.py
@@ -1,15 +1,11 @@
 import asyncio
-import time
-import re
 from datetime import datetime
 from datetime import timedelta
-from database import engine_db
-from database import StoppedTickets, ExpiresTickets
 from envparse import Env
 from log import logger
-from typing import Coroutine, Awaitable
+from typing import Awaitable
 from ws_server import ServerServices
-from sqlalchemy import select, update, insert
+from analytics import Analytics
 
 
 env = Env()
@@ -21,12 +17,12 @@ stopped_filter = env.str("FILTER_STOPPED")
 arrears_filter = env.str("FILTER_ARREARS")
 
 
-class Analytics:
+class TaskManager:
     """
-    Класс для анализа тикетов из системы NTTM (просроченные, приостановленные):
+    Класс для менеджмента клиент-серверных задач (получение дашборда/тикета):
     1. Класс обращается к методам класса ServerServices для создания заданий
-    2. Получая информацию по тикету, вызывается нужный метод класс
-    3. После анализа тикета результат проверки добавляется в БД
+    2. Класс ServerServices создаёт задания для подключенных клиентов и отправляет их
+    3. Получая в ответе информацию по тикету, вызывается нужный метод из класса Analytics
     """
 
     def __init__(self, stopped: str, arrears: str) -> None:
@@ -34,196 +30,6 @@ class Analytics:
         self._stopped_tickets = []
         self._stopped_filter = stopped
         self._arrears_filter = arrears
-        self._database = engine_db
-        
-    @staticmethod
-    def analise(ticket: dict) -> str:
-        def expires(task: dict, counter_sla: int, sla: int) -> int | str:
-            group = 'SLA не превышен'
-            time_create = datetime.strptime(task['createTs'][0:19], format_date)
-            time_close = datetime.strptime(task['completionDate'][0:19], format_date)
-
-            time_work_on_request = (time_close - time_create).total_seconds() / 60
-            counting_sla = counter_sla + time_work_on_request
-
-            if counting_sla >= sla:
-                if not task.get('assignmentDate'):
-                    time_assign = time_create
-                else:
-                    time_assign = datetime.strptime(task['assignmentDate'][0:19], format_date)
-
-                group = task['taskExecutorDTO']['execUnitName']
-
-                if task.get('taskComments') and len(task['taskComments'][-1]['comment']) > 0:
-                    end_comment = task['taskComments'][-1]['comment']
-                    parasitic_data = ("<p>", "</p>", "&nbsp;", "<br>", "<strong>",
-                                      "</strong>", "&gt;", "&lt;", "'")
-
-                    for old in parasitic_data:
-                        end_comment = end_comment.replace(old, ' ')
-                else:
-                    end_comment = "Комментарий отсутствует"
-
-                analise_info.update({'Результат': f"SLA превышен",
-                                     'Просрочено на группе': group,
-                                     'Номер запроса': task['taskNumber'],
-                                     'Время создания запроса': time_create.strftime(format_date),
-                                     'Время принятия в работу': time_assign.strftime(format_date),
-                                     'Время закрытия запроса': time_close.strftime(format_date),
-                                     'Время реакции': time_assign - time_create,
-                                     'Время решения': time_close - time_assign,
-                                     'Время всё': time_close - time_create,
-                                     'Последний комментарий (task)': end_comment})
-
-                if group == 'Вендор (ДЭФИР)' and task.get('foreignTicketId'):
-                    group = f"{task['providerName']}"
-                elif group == 'Провайдер' and task.get('foreignTicketId'):
-                    group = f"Провайдер ({task['providerName']})"
-
-            return counting_sla, group
-
-        if ticket.get('closeDate'):
-            close_date = str(ticket['closeDate']).split("T")[0]
-        else:
-            close_date = 'Не удалось определить'
-
-        analise_info = {}
-        format_date = "%Y-%m-%dT%H:%M:%S"
-        ticket_sla = int(ticket['ola']['ksSla'])
-        count_sla = 0
-
-        if ticket['status'] == 'В работе':
-            if ticket['tasks'][-1]['typeName'] == 'Ожидание':
-                analise_info['Результат'] = 'ТТ в приостановке'
-            else:
-                analise_info['Результат'] = 'ТТ в работе'
-
-        elif ticket['status'] == 'Закрыт':
-            for ticket_task in ticket['tasks']:
-                if ticket_task['typeName'] not in ('Ожидание', 'Запрос клиента'):
-                    count_sla, expire_group = expires(ticket_task, count_sla, ticket_sla)
-                    if expire_group:
-                        break
-
-                if ticket_task['typeName'] == 'Ожидание':
-                    execution = ticket_task['taskExecutorDTO']['execUnitName']
-                    if execution in ('Решение сетевого ТТ', 'Решение базового ТТ'):
-                        count_sla, expire_group = expires(ticket_task, count_sla, ticket_sla)
-                        if expire_group:
-                            break
-            else:
-                analise_info['Результат'] = 'SLA не превышен'
-        else:
-            logger.info(f"В анализе найден неизвестный статус ТТ {ticket['id']} {ticket['status']}")
-
-        text = '\n'.join(map(lambda x: f"{x[0]}: {x[1]}", analise_info.items()))
-        return text, expire_group, close_date
-
-    def stopped_report(self, stopped_tickets: list) -> list[dict]:
-        def time_format(date: datetime) -> str:
-            return str(datetime.strptime(date[0:19], "%Y-%m-%dT%H:%M:%S"))
-
-        try:
-            result = []
-            for ticket in stopped_tickets:
-                logger.warning(f"Проверяю TT {ticket['id']}")
-                ticket_sla = time_format(ticket['ola']['kdSLA'])
-                ticket_dump = ticket['tasks']
-
-                for task in reversed(ticket_dump):
-                    if task['typeName'] == 'Ожидание':
-                        index = ticket_dump.index(task)
-
-                        author = ticket_dump[index - 1]['taskExecutorDTO']['executorName']
-                        subdivision_requirement = ticket_dump[index - 2]['targetUnitName']
-                        comment = re.sub(r'\<[^>]*\>', '', ticket_dump[index - 1]['closeComment'])
-
-                        ticket_info = {
-                            "Номер запроса": task['taskNumber'],
-                            "Дата согласования приостановки": time_format(ticket_dump[index - 1]['createTs']),
-                            "Дата начала приостановки": time_format(task['createTs']),
-                            "Дата конца приостановки": time_format(task['suspendDate']),
-                            "Кто приостановил": author,
-                            "Последний коммент": comment,
-                            "Кто запросил приостановку": subdivision_requirement,
-                            "SLA (до которого)": ticket_sla
-                        }
-                        result.append(ticket_info)
-                        break
-
-            with self._database.connect() as session:
-                request = select(StoppedTickets.task_number)
-                tickets_task = session.execute(request)
-                session.commit()
-                
-            tickets_id = [task[0] for task in tickets_task]
-
-            for ticket_data in result:
-                data = list(ticket_data.values())
-                ticket_id_num = data[0]
-                task_id = str(data[0]).split("-")[0]
-
-                if ticket_id_num in tickets_id:
-                    with self._database.connect() as session:
-                        request = update(StoppedTickets).where(StoppedTickets.task_number == task_id).values((*data,))
-                        session.execute(request)
-                        session.commit()
-
-                    logger.warning(f"Обновлён ТТ {ticket_id_num}")
-                else:
-                    with self._database.connect() as session:
-                        request = insert(StoppedTickets).values((*data,))
-                        session.execute(request)
-                        session.commit()
-
-                    logger.warning(f"Добавлен ТТ {ticket_id_num}")
-            return result
-
-        except Exception as err:
-            logger.error(f"Ошибка в отчёте приостановок: {err}", exc_info=True)
-
-    def arrears_report(self, arrears_tickets: list) -> list[dict]:
-        def time_formats(ticket_data: dict) -> datetime:
-            formate = '%Y-%m-%d'
-            now = datetime.now()
-
-            date_y = datetime.strptime((now - timedelta(days=1)).strftime(formate), formate)
-            date_dby = datetime.strptime((now - timedelta(days=2)).strftime(formate), formate)
-
-            time_str = str(ticket_data['tasks'][-1]['completionDate']).split('T')[0]
-            date_close = datetime.strptime(time_str, formate)
-            return date_y, date_dby, date_close
-
-        try:
-            result = []
-            for ticket in arrears_tickets:
-                ticket_number = ticket['id']
-                logger.warning(f"Проверяю TT {ticket_number}")
-
-                yesterday, db_yesterday, close = time_formats(ticket)
-
-                if yesterday >= close > db_yesterday:
-                    text, group, close_date = self.analise(ticket)
-    
-                    with self._database.connect() as session:
-                        request = insert(ExpiresTickets).values((ticket_number, group, text, close_date))
-                        session.execute(request)
-                        session.commit()
-
-                    result.append(
-                        {
-                        "ticket": ticket,
-                        "resp": group,
-                        "comment": text,
-                        "date": close_date
-                        }
-                    )
-                else:
-                    result.append({"ticket": ticket, "comment": "eliminated"})
-            return result
-
-        except Exception as err:
-            logger.error(f"Ошибка в отчёте просрочек: {err}", exc_info=True)
 
     async def get_dashboard(self, services: ServerServices, key_filter: str) -> list[int]:
         incidents = []
@@ -276,8 +82,9 @@ class Analytics:
 
 
 async def core() -> Awaitable:
-    analyst = Analytics(stopped_filter, arrears_filter)
+    manager = TaskManager(stopped_filter, arrears_filter)
     service = ServerServices(port)
+    analyst = Analytics()
 
     successfully = False
     date_event = datetime.now() - timedelta(seconds=1)
@@ -288,9 +95,9 @@ async def core() -> Awaitable:
             
             if now.hour < 3 and not successfully:
                 if service.nttm.check_client():
-                    tickets = await analyst.get_dashboard(service, arrears_filter)
+                    tickets = await manager.get_dashboard(service, arrears_filter)
                     if tickets:
-                        result = await analyst.task_creator(service, tickets, arrears_filter)
+                        result = await manager.task_creator(service, tickets, arrears_filter)
                         expires = analyst.arrears_report(result)
                         if expires and len(tickets) == len(expires):
                             successfully = True
@@ -299,9 +106,9 @@ async def core() -> Awaitable:
 
             if now > date_event:
                 if service.nttm.check_client():
-                    tickets = await analyst.get_dashboard(service, stopped_filter)
+                    tickets = await manager.get_dashboard(service, stopped_filter)
                     if tickets:
-                        result = await analyst.task_creator(service, tickets, stopped_filter)
+                        result = await manager.task_creator(service, tickets, stopped_filter)
                         analyst.stopped_report(result)
                         date_event = now + timedelta(seconds=interval)
 
